f539cf60a046916c6d4190e70db3f5eb75fd5d57
Minye Zhang <minyez.physchem@gmail.com>
2023-06-08 11:21:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Useful tools but maybe missing on HPC platform.
+
+- [ripgrep](https://github.com/BurntSushi/ripgrep)
+- [fd-find](https://github.com/sharkdp/fd)
+- [fzf](https://github.com/junegunn/fzf)
+
+## Usage
+
+Make sure your platform can reach GitHub and
+you have `git` and `wget` on it.
+
+Clone this repository to `/path/to/hpc_tools`, run
+
+```shell
+cd ~/hpc_tools
+./fetch_tarball.sh
+```
+
+Or run it locally and upload to `/path/to/hpc_tools` at the server.
+Then copy the modulefile (`modulefile`) to somewhere handled by
+environment module.
+You need to edit variable `hpc_tools_home` therein to `/path/to/hpc_tools`
+if the path is not `$HOME/hpc_tools`.
+
+## [License](./LICENSE)
+
+The MIT License (MIT).
---
+++ fetch_tarball.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+WGET_CMD="wget -nv --show-progress --progress=bar:force"
+
+fetch_ripgrep() {
+  if [[ -d ripgrep ]]; then
+    echo "ripgrep already fetched" && return
+  fi
+  ripgrepver="13.0.0"
+  ripgrepdir="ripgrep-$ripgrepver-x86_64-unknown-linux-musl"
+  ripgreptgz="$ripgrepdir.tar.gz"
+  ripgrepurl="https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/$ripgreptgz"
+
+  [[ -f "$ripgreptgz" ]] || $WGET_CMD "$ripgrepurl"
+  tar -zxf "$ripgreptgz"
+  mv "$ripgrepdir" ripgrep
+}
+
+fetch_fd() {
+  if [[ -d fd ]]; then
+    echo "fd already fetched" && return
+  fi
+  fdver="v8.7.0"
+  fddir="fd-$fdver-x86_64-unknown-linux-musl"
+  fdtgz="$fddir.tar.gz"
+  fdurl="https://github.com/sharkdp/fd/releases/download/$fdver/$fdtgz"
+
+  [[ -f "$fdtgz" ]] || $WGET_CMD "$fdurl"
+  tar -zxf "$fdtgz"
+  mv "$fddir" fd
+}
+
+fetch_fzf() {
+  if [[ -d fzf ]]; then
+    echo "fzf already fetched" && return
+  fi
+  fzfver="0.41.1"
+  fzfdir="fzf-$fzfver-linux_amd64"
+  fzftgz="$fzfdir.tar.gz"
+  fzfurl="https://github.com/junegunn/fzf/releases/download/$fzfver/$fzftgz"
+  [[ -f "$fzftgz" ]] || $WGET_CMD "$fzfurl"
+  mkdir -p fzf
+  tar -C fzf -zxf "$fzftgz"
+}
+
+fetch_ripgrep
+fetch_fd
+fetch_fzf
